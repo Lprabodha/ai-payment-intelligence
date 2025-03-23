435d2fd95ec6611e6eabe69e25cf58e3939cb699
lprabhoda1998 <lahiru@epicy.com>
2025-03-23 07:40:07 +0000

    docker compose : Recteate DOcker compose

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,6 +2,10 @@
 
 echo "🚀 Starting Cron and FastAPI..."
 
+touch /src/logs/cron.log
+ls -h /sr
+
 cron &
 
-uvicorn api.main:app --host 0.0.0.0 --port 8010 --reload
+
+uvicorn api.main:app --host 0.0.0.0 --port 8010 --reload || (pip install --upgrade pip && pip install -r requirements.txt && uvicorn api.main:app --host 0.0.0.0 --port 8010 --reload)
